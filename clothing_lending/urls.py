4818df90db9c2a6e6e317cade480cb5ad84a5da6
SanikaRewatkar2 <sanika_rewatkar@icloud.com>
2025-02-20 18:24:25 -0500

    Added template, updated urls

--- clothing_lending/urls.py
+++ clothing_lending/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from . import views
+from . import views, include
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    #path("", views.index, name="index"),
+    path('', TemplateView.as_view(template_name="index.html")),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view()),
 	path("catalog/", views.catalog, name = "catalog"),
 	path("checkout/", views.checkout, name = "checkout"),
 ]
