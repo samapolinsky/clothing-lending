53172781d9a7d4c6f5dc9f76111b9006dc1440b2
kadennguyen0329 <94870428+kadennguyen0329@users.noreply.github.com>
2025-02-20 20:26:19 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'clothing_lending',
     'allauth',
     'allauth.account',
@@ -76,7 +77,7 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
-SITE_ID = 2
+SITE_ID = 3
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
