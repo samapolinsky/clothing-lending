aa613f5d90db43212afac574f0a020a6f26ef3fc
kadennguyen0329 <94870428+kadennguyen0329@users.noreply.github.com>
2025-03-21 23:48:21 -0400

    patron table updated
    to do: update tables when librarian promoted

--- clothing_lending/signals.py
+++ clothing_lending/signals.py
@@ -1,7 +1,7 @@
 # filepath: c:\Users\kaden\CS3240\Lending\project-b-23\clothing_lending\signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import User, Librarian
+from .models import User, Librarian, Patron
 
 @receiver(post_save, sender=User)
 def create_or_update_librarian(sender, instance, created, **kwargs):
@@ -15,3 +15,16 @@ def save_librarian(sender, instance, **kwargs):
             instance.librarian.save()
         except Librarian.DoesNotExist:
             Librarian.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def create_or_update_patron(sender, instance, created, **kwargs):
+    if instance.user_type == 2:
+        Patron.objects.get_or_create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_patron(sender, instance, **kwargs):
+    if instance.user_type == 2:
+        try:
+            instance.patron.save()
+        except Patron.DoesNotExist:
+            Patron.objects.create(user=instance)
