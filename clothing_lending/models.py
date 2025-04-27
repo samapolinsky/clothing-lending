b74508df8de72b2fd73682f7ad401e42b9cb979f
SanikaRewatkar2 <sanika_rewatkar@icloud.com>
2025-04-27 00:20:01 -0400

    Added character limit to collection description.

--- clothing_lending/models.py
+++ clothing_lending/models.py
@@ -34,7 +34,7 @@ class Patron(models.Model):
 class Collection(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='collections')
